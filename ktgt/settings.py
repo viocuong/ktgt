5aa8f7749aa97447cbbaee201857987ae37c8e2d
viocuong <cuong1999ads@gmail.com>
2021-05-20 10:03:36 +0700

    up

--- ktgt/settings.py
+++ ktgt/settings.py
@@ -114,11 +114,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
-STATICFILES_DIRS=[
-    BASE_DIR / "static",
+# STATICFILES_DIRS=[
+#     BASE_DIR / "static",
 
-]
+# ]
