364774432c15aa4b0b414258214816992a88a76a
viocuong <cuong1999ads@gmail.com>
2021-05-21 14:34:29 +0700

    up

--- ktgt/settings.py
+++ ktgt/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
