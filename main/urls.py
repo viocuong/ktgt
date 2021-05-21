0a75295bfebf134999740cb3cc7880b3fa2eb0fa
viocuong <cuong1999ads@gmail.com>
2021-05-21 14:56:26 +0700

    up

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     path("",views.index,name = 'index'),
-    path("home",views.home,name = 'home')
+    #path("home",views.home,name = 'home')
 ]
