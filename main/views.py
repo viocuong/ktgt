02736515775ea723f365d2efbaf108880046632d
viocuong <cuong1999ads@gmail.com>
2021-05-23 08:40:24 +0700

    up

--- main/views.py
+++ main/views.py
@@ -31,7 +31,8 @@ def index(request):
             f.close()
             if tool == 'wavsteg':
                 sulfix = '_wavsteg.wav'
-                os.system(f"stegolsb wavsteg -h -i media/{fileName}.wav  -s media/message.txt -o media/{fileName}_wavsteg.wav -n 1")
+                os.system("stegolsb --help")
+                # os.system(f"stegolsb wavsteg -h -i media/{fileName}.wav  -s media/message.txt -o media/{fileName}_wavsteg.wav -n 1")
                 # output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
                 url = f'/media/{fileName}_wavsteg.wav' 
             elif tool == 'mp3stego':
